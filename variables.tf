51d287492baccc9123b822c37cc929e678a4c100
Nitika Thingalaya <110655416+NitikaThingalaya-pki@users.noreply.github.com>
2024-07-18 20:35:47 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -25,32 +25,6 @@ variable "s3_bucket_name" {
 
 
 
-variable "sensitive_data" {
-  description = "Sensitive PII and secrets data"
-  type        = string
-  default     = jsonencode({
-    user = {
-      firstName             = "John"
-      lastName              = "Doe"
-      email                 = "john.doe@example.com"
-      phoneNumber           = "+1234567890"
-      socialSecurityNumber  = "123-45-6789"
-      address               = {
-        street  = "123 Main St"
-        city    = "Anytown"
-        state   = "Anystate"
-        zipCode = "12345"
-      }
-    }
-    credentials = {
-      apiKey      = "ABCD1234EFGH5678IJKL91011MNOP"
-      password    = "s3cr3tP@ssw0rd"
-      accessToken = "ya29.A0ARrdaM-pqDl3a9mO1qLjDzV5VzOzL"
-      privateKey  = "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASC...END PRIVATE KEY-----"
-    }
-  })
-}
-
 
 variable "name" {
   description = "Name of EC2 Intance"
