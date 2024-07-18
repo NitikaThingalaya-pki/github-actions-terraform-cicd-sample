d3d5ab457ea154c5d8530a8bf6e22d1e9befe428
Nitika Thingalaya <110655416+NitikaThingalaya-pki@users.noreply.github.com>
2024-07-18 18:50:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,11 +20,26 @@ data "aws_ami" "amazon_linux" {
   }
 }
 
+resource "aws_network_interface" "example" {
+  subnet_id = var.subnet_id
+
+  # Disable public IP assignment
+  associate_public_ip_address = false
+
+  tags = {
+    Name = "example-nic"
+  }
+}
+
 resource "aws_instance" "example" {
   ami           = data.aws_ami.amazon_linux.id
   instance_type = var.instance_type
   key_name      = var.key_name
-  ecs_associate_public_ip_address = "false"
+
+  network_interface {
+    network_interface_id = aws_network_interface.example.id
+    device_index         = 0
+  }
 
   tags = {
     Name = var.name
@@ -37,7 +52,6 @@ resource "aws_instance" "example" {
               sudo systemctl start httpd
               sudo systemctl enable httpd
               EOF
-
 
   lifecycle {
     create_before_destroy = true
